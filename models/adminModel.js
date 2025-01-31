40b0abbe33f53d4a9e4f33be3116a271a44d0c23
himayrobsarr <serganimon@gmail.com>
2025-01-31 10:26:31 -0500

    feat: :ambulance: "emergencia por una imagen" socorro

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,9 +1,10 @@
 const db = require("../helpers/conexion");
 
+
 class adminModel {
     static async getAllregister() {
         const query = 'SELECT COUNT(*) AS total_registros FROM CAMPER;';
-        const result = await conexion.query(query);
+        const result = await db.query(query);
         if (!result.data || result.data.length === 0) {
             throw new Error('No se encontraron registros');
         }
@@ -11,7 +12,8 @@ class adminModel {
     }
 
     static async getAllIncomplete() {
-        const query = `SELECT COUNT(c.id) AS total_incompletos
+        const query = `SELECT c.id, c.user_id, c.title, c.history, c.about, c.main_video_url, 
+                              c.full_name, c.profile_picture, c.status
                        FROM CAMPER c
                        LEFT JOIN CAMPER_PROJECT cp ON c.id = cp.camper_id
                        LEFT JOIN CAMPER_MERIT cm ON c.id = cm.user_id
@@ -28,13 +30,15 @@ class adminModel {
                              AND (cp.camper_id IS NOT NULL OR
                                   cm.user_id IS NOT NULL OR
                                   d.camper_id IS NOT NULL OR
-                                  tv.camper_id IS NOT NULL);`;
+                                  tv.camper_id IS NOT NULL)
+                       GROUP BY c.id, c.user_id, c.title, c.history, c.about, c.main_video_url, 
+                                c.full_name, c.profile_picture, c.status;`;
 
-        const result = await conexion.query(query);
+        const result = await db.query(query);
         if (!result.data || result.data.length === 0) {
             throw new Error('No se encontraron registros incompletos');
         }
-        return result.data[0].total_incompletos;
+        return result.data; 
     }
 }
 
