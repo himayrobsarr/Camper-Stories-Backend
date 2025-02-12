c296188492ab666b155097576e8bf25ca3365aac
santilaguado <santiagolaguado57@gmail.com>
2025-02-12 18:50:52 -0500

    fix: :ambulance: Update CORS options to remove trailing slash from campuslands URL

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -8,7 +8,7 @@ const corsOptions = {
             'https://camper-stories.vercel.app' ,    // Tu dominio en producción
             'https://camperstories.vercel.app',
             'https://admin-camper-stories.vercel.app',
-            'https://campuslands.vercel.app/'
+            'https://campuslands.vercel.app'
         ];
         
         // Permitir peticiones sin origen (como las de Postman)
