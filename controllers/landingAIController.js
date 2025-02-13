2696fabaee6a8ff1846129c89186851d9685697c
santilaguado <santiagolaguado57@gmail.com>
2025-02-13 18:13:43 -0500

    feat: :sparkles: Nuevo campo en AI Controller

--- controllers/landingAIController.js
+++ controllers/landingAIController.js
@@ -3,9 +3,9 @@ const landingAIModel = require('../models/landingAIModel');
 const landingAIController = {
     saveRegisteredInfo: async (req, res) => {
         try {
-            const { name, lastname, email, phone, document, payment_reference, payment_date } = req.body;
+            const { name, lastname, email, phone, document, payment_reference, payment_date, selected_course } = req.body;
 
-            if (!name || !lastname || !email || !phone || !document) {
+            if (!name || !lastname || !email || !phone || !document || !payment_reference || !payment_date || !selected_course) {
                 return res.status(400).json({ error: "Faltan datos requeridos" });
             }
 
@@ -16,10 +16,11 @@ const landingAIController = {
                 phone,
                 document,
                 payment_reference,
-                payment_date
+                payment_date,
+                selected_course
             });
 
-            return res.status(200).json({ message: "Registro guardad exitosamente" });
+            return res.status(200).json({ message: "Registro guardadito exitosamente" });
         } catch (error) {
             console.error("Error guardando la información:", error);
             return res.status(500).json({ error: "Error interno del servidor" });
--- models/landingAIModel.js
+++ models/landingAIModel.js
@@ -1,16 +1,16 @@
 const db = require("../helpers/conexion");
 
 const landingAIModel = {
-    saveRegisteredInfo: async ({ name, lastname, email, phone, document, payment_reference, payment_date }) => {
+    saveRegisteredInfo: async ({ name, lastname, email, phone, document, payment_reference, payment_date, selected_course }) => {
       try {
 
         const formattedDate = new Date(payment_date).toISOString().slice(0, 19).replace('T', ' ');
   
         const query = `
-          INSERT INTO INSCRIPCIONES_IA (name, lastname, email, phone, document, payment_reference, payment_date)
+          INSERT INTO INSCRIPCIONES_IA (name, lastname, email, phone, document, payment_reference, payment_date, selected_course)
           VALUES (?, ?, ?, ?, ?, ?, ?)
         `;
-        const values = [name, lastname, email, phone, document, payment_reference, formattedDate];
+        const values = [name, lastname, email, phone, document, payment_reference, formattedDate, selected_course];
   
         const { status, data } = await db.query(query, values);
         if (status === 200) {
