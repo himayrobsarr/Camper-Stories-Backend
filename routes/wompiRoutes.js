d05f58bd9713c0adabb6f06538d9c8cb74a216ae
santilaguado <santiagolaguado57@gmail.com>
2025-02-19 15:41:24 -0500

    fix: :ambulance: Quite la proteccion de los endpoints de Wompi.

--- routes/wompiRoutes.js
+++ routes/wompiRoutes.js
@@ -5,20 +5,20 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const validateSubscriptionData = require("../middlewares/validateSubscriptionData");
 
 // Rutas para suscripciones
-router.post("/init-subscription",authMiddleware,validateSubscriptionData,WompiController.initSubscription); 
-router.post("/process-subscription", authMiddleware,WompiController.processSubscription);
+router.post("/init-subscription", validateSubscriptionData,WompiController.initSubscription); 
+router.post("/process-subscription", WompiController.processSubscription);
 router.post("/receive-webhook", WompiController.receiveWebhook);
-router.post("/generate-signature", authMiddleware, WompiController.generateSignature);
+router.post("/generate-signature", WompiController.generateSignature);
 
 
 // Ruta para procesar los webhooks de Wompi
 router.post('/save-info', WompiController.savePaymentInfo);
 
 // Nuevas rutas
-router.get("/subscriptions", authMiddleware, WompiController.getUserSubscriptions);
-router.get("/subscription/:subscriptionId", authMiddleware, WompiController.getSubscription);
-router.delete("/subscription/:subscriptionId", authMiddleware, WompiController.cancelSubscription);
-router.put("/subscription/:subscriptionId/payment-method", authMiddleware, WompiController.updatePaymentMethod);
-router.get("/subscription/:subscriptionId/payments", authMiddleware, WompiController.getSubscriptionPayments);
+router.get("/subscriptions", WompiController.getUserSubscriptions);
+router.get("/subscription/:subscriptionId", WompiController.getSubscription);
+router.delete("/subscription/:subscriptionId", WompiController.cancelSubscription);
+router.put("/subscription/:subscriptionId/payment-method", WompiController.updatePaymentMethod);
+router.get("/subscription/:subscriptionId/payments", WompiController.getSubscriptionPayments);
 
 module.exports = router;
